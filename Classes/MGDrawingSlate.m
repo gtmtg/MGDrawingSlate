ff3965dd83441a8389b84d1980f12152f5bfade6
Mihir Garimella <apps@mihirgarimella.com>
2013-02-25 21:17:01 -0500

    Added storyboard initialization, cleaned up code

--- Classes/MGDrawingSlate.m
+++ Classes/MGDrawingSlate.m
@@ -15,10 +15,23 @@
 
 - (id)initWithFrame:(CGRect)frame
 {
-    
     self = [super initWithFrame:frame];
+    [self initializeSlate];
+    
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    [self initializeSlate];
+    
+    return self;
+}
+
+- (void)initializeSlate
+{
     if (self) {
-        
         //Initialize MGDrawingSlate and set default values
         self.backgroundColor = [UIColor whiteColor];
         drawingPath = [[UIBezierPath alloc]init];
@@ -26,53 +39,42 @@
         drawingPath.miterLimit = 0;
         drawingPath.lineWidth = 4; //Default line weight - change with changeLineWeightTo: method.
         drawingColor = [UIColor blackColor]; //Default color - change with changeColorTo: method.
-        
     }
-    return self;
-    
 }
 
 #pragma mark - Customization Methods
 
 //Call from view controller to change the line weight of the drawing path. Alternatively, just change [drawingSlate]->drawingPath.lineWidth.
-- (void)changeLineWeightTo:(NSInteger)weight {
-    
+- (void)changeLineWeightTo:(NSInteger)weight
+{
     drawingPath.lineWidth = weight;
-    
 }
 
 //Call from view controller to change the color of the drawing path. Alternatively, just change [drawingSlate]->drawingColor.
-- (void)changeColorTo:(UIColor *)color {
-    
+- (void)changeColorTo:(UIColor *)color
+{
     drawingColor = color;
-    
 }
 
 #pragma mark - Drawing Methods
 
 - (void)drawRect:(CGRect)rect
 {
-    
     [drawingColor setStroke];
     [drawingPath strokeWithBlendMode:kCGBlendModeNormal alpha:1.0];
-    
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
     UITouch *touch = [[touches allObjects] objectAtIndex:0];
     [drawingPath moveToPoint:[touch locationInView:self]];
-    
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
     UITouch *touch = [[touches allObjects] objectAtIndex:0];
     [drawingPath addLineToPoint:[touch locationInView:self]];
     [self setNeedsDisplay];
-    
 }
 
 @end
